f0ebce466e1844e3bfa06a726b75d2ff4daba753
Darwin Subramaniam <darwin.subramaniam@keysight.com>
2023-01-12 15:12:55 +0800

    Add Report

---
+++ SampleOpenTAPExecutor/Program.cs
@@ -0,0 +1,30 @@
+﻿using SampleOpenTAPExecutor;
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Starting OpenTAP");
+
+SearchPlugin().Wait();
+
+for (int i = 0; i < 50; i++)
+{
+    Console.WriteLine(i.ToString());
+    //SearchPlugin().Wait();
+    var testplan = SampleTestPlanBuilder.GetTestPlan();
+    var testPlanRunTask = testplan.ExecuteAsync();
+    testPlanRunTask.Wait();
+}
+
+Task SearchPlugin()
+{
+    var pluginSearch = OpenTap.PluginManager.SearchAsync();
+
+    pluginSearch.ContinueWith(x =>
+    {
+        var plugins = OpenTap.PluginManager.GetAllPlugins();
+    });
+    return pluginSearch;
+}
+
+
---
+++ SampleOpenTAPExecutor/SampleTestPlan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTap;
+
+namespace SampleOpenTAPExecutor
+{
+    public class SampleTestPlanBuilder
+    {
+        public static TestPlan GetTestPlan()
+        {
+            var logTestStep = new OpenTap.Plugins.BasicSteps.LogStep
+            {
+                LogMessage = "Hi Darwin"
+            };
+
+            var delayTestStep = new OpenTap.Plugins.BasicSteps.DelayStep
+            {
+                DelaySecs = 3
+            };
+
+            var testplan = new TestPlan();
+            testplan.ChildTestSteps.Add(logTestStep);
+            testplan.ChildTestSteps.Add(delayTestStep);
+
+            return testplan;
+        }
+    }
+}
